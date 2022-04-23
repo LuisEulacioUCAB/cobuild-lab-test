662fac4150c54bdf4c3cd9215e8c6132ee95da48
LuisEulacio <luiseulacio1255@gmail.com>
2022-04-23 16:57:43 -0400

    test

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
-const newCode = (code)=> console.log('newCode:'+ code);
+const moment = require('moment');
+
+const newCode = (code)=> {
+
+    const since = `${startMonth}T00:00:00Z`;
+    const to = `${endMonth}T23:59:00Z`;
+    console.log('newCode:' + code);
+    console.log('startMonth:' + startMonth);
+};
+
+const a = moment([2012]);
+const b = moment(a);
+a.year();
+b.year(); // 2012
+
+console.log('a', a);
+console.log('b', b);
+
+newCode("Natalia");
